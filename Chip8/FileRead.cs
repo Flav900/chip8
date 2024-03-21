fbedcd20450dd3511d4f6bbb764a247c94d4c29a
Flav <flavio.mattiello900@gmail.com>
2024-03-21 17:42:18 +0200

    File reading test

--- Chip8/FileRead.cs
+++ Chip8/FileRead.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,31 @@ namespace Chip8
 		public FileRead() {
 
 
-			Console.WriteLine("Test");
+			Console.WriteLine("Test Read");
 			Console.ReadLine();
+
+
+
+			// big endian is left to right, which is what chip8 uses
+			// little endian is right to left 
+			//chip 8 reads 2 bytes in at a time
+
+			string filePath = "D:/Projects/Chip8/IBM_Logo.ch8";
+
+			using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+			{
+				byte[] buffer = new byte[2];
+
+				while (fs.Read(buffer, 0, buffer.Length) == buffer.Length)
+				{
+				
+					int value = BitConverter.ToUInt16(buffer, 0);
+					string hexString = value.ToString("X4"); // "X4" for 4 digits
+
+					Console.WriteLine(hexString);
+					Console.ReadLine();
+				}
+			}
 		}
 
 	}
