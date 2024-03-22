ca1faa88efe00bd0b48f1b2d8fc09dd242508a92
Flav <flavio.mattiello900@gmail.com>
2024-03-22 14:00:21 +0200

    Correctly loading the rom now at index 512
    Next I need to add in the fonts

--- Chip8/Chip8Core.cs
+++ Chip8/Chip8Core.cs
@@ -15,7 +15,7 @@ namespace Chip8
 
 	//	ushort opCode;
 		ushort index;
-		ushort programCounter = 0;
+		ushort programCounter = 512;
 
 		byte[] memory = new byte[4096];
 
@@ -33,6 +33,10 @@ namespace Chip8
 
 		public void init()
 		{
+			// I need to add fonts and other functions in the first 512
+			//512 onwards where I'm supposed to load rom
+
+
 			readRom("G:/Projects/Chip8/IBM_Logo.ch8");
 
 		}
@@ -44,118 +48,127 @@ namespace Chip8
 
 		public void doCycle()
 		{
-			//Fetch
-			byte[] chunk = { memory[programCounter], memory[programCounter + 1] };
-			bool jumped = false;
 
-
-			// Convert it to Big Endian if its in little endian, should fix read bug
-			if (BitConverter.IsLittleEndian)
+			//Console.WriteLine("PC: " + programCounter);
+			try
 			{
-				Array.Reverse(chunk);
-			}
-
-			string opCode = BitConverter.ToUInt16(chunk, 0).ToString("X4"); // "X4" for 4 digits
-
-			Console.WriteLine(opCode);
+				//Fetch
+				byte[] chunk = { memory[programCounter], memory[programCounter + 1] };
+				bool jumped = false;
 
 
-			//Decode
-
-			if (opCode.Equals("00E0")) {
-				Console.WriteLine("Clearing Screen");
-				clearScreen();
-			} 
-			else if (opCode.StartsWith("A")) {
-			
-
-				string nnn = opCode.Substring(1);
-
-				Console.WriteLine("Set Index to "+nnn);
-
-				index = (ushort)Convert.ToInt32(nnn, 16);
-
-			}
-			else if (opCode.StartsWith("1"))
-			{
-				//Set the PC to nnn
-				string nnn = opCode.Substring(1);
-
-				Console.WriteLine("Set the Program Counter to "+nnn);
-			
-				programCounter = (ushort)Convert.ToInt32(nnn, 16);
-				jumped = true;
-
-			}
-			else if (opCode.StartsWith("6"))
-			{
-				int x = Int32.Parse(opCode.Substring(1,1));
-
-				string nn = opCode.Substring(2);
-
-				Console.WriteLine("Set Cpu Register " + x +" to "+ nn);
-
-				//set register VX
-				cpuRegisters[x] = Convert.ToByte(nn, 16);
-
-
-			}
-			else if (opCode.StartsWith("7"))
-			{
-				//add value to register VX
-				int x = Int32.Parse(opCode.Substring(1, 1));
-
-				string nn = opCode.Substring(2);
-
-
-				Console.WriteLine("Add "+ nn+" to Cpu Register " + x);
-
-				int sum = cpuRegisters[x] + Convert.ToByte(nn, 16);
-
-				Console.WriteLine("sum " + sum);
-
-				//need to check what happens when overflow is detected
-				if (sum > 255)
+				// Convert it to Big Endian if its in little endian, should fix read bug
+				if (BitConverter.IsLittleEndian)
 				{
-
-					Console.WriteLine("Overflow");
-			
-					sum = sum % 256; //wraps around it, for now
-
+					Array.Reverse(chunk);
 				}
 
-				cpuRegisters[x] = (byte)sum;
+				string opCode = BitConverter.ToUInt16(chunk, 0).ToString("X4"); // "X4" for 4 digits
+
+				Console.WriteLine(opCode);
 
 
-			}
-			else if (opCode.StartsWith("D"))
+				//Decode
+
+				if (opCode.Equals("00E0"))
+				{
+					Console.WriteLine("Clearing Screen");
+					clearScreen();
+				}
+				else if (opCode.StartsWith("A"))
+				{
+
+
+					string nnn = opCode.Substring(1);
+
+					Console.WriteLine("Set Index to " + nnn);
+
+					index = (ushort)Convert.ToInt32(nnn, 16);
+
+				}
+				else if (opCode.StartsWith("1"))
+				{
+					//Set the PC to nnn
+					string nnn = opCode.Substring(1);
+
+					Console.WriteLine("Set the Program Counter to " + nnn);
+
+					programCounter = (ushort)Convert.ToInt32(nnn, 16);
+					jumped = true;
+
+				}
+				else if (opCode.StartsWith("6"))
+				{
+					int x = Int32.Parse(opCode.Substring(1, 1));
+
+					string nn = opCode.Substring(2);
+
+					Console.WriteLine("Set Cpu Register " + x + " to " + nn);
+
+					//set register VX
+					cpuRegisters[x] = Convert.ToByte(nn, 16);
+
+
+				}
+				else if (opCode.StartsWith("7"))
+				{
+					//add value to register VX
+					int x = Int32.Parse(opCode.Substring(1, 1));
+
+					string nn = opCode.Substring(2);
+
+
+					Console.WriteLine("Add " + nn + " to Cpu Register " + x);
+
+					int sum = cpuRegisters[x] + Convert.ToByte(nn, 16);
+
+					Console.WriteLine("sum " + sum);
+
+					//need to check what happens when overflow is detected
+					if (sum > 255)
+					{
+
+						Console.WriteLine("Overflow");
+
+						sum = sum % 256; //wraps around it, for now
+
+					}
+
+					cpuRegisters[x] = (byte)sum;
+
+
+				}
+				else if (opCode.StartsWith("D"))
+				{
+					//Draw
+					int x = Int32.Parse(opCode.Substring(1, 1));
+					int y = Int32.Parse(opCode.Substring(2, 1));
+					char n = opCode[3];
+
+					Console.WriteLine("Draw " + n + " at x:" + x + ", y:" + y);
+
+
+					display[x, y] = n;
+
+				}
+				else
+				{
+					Console.WriteLine("Unknown opCode: " + opCode);
+				}
+
+
+
+
+				if (!jumped)
+				{
+
+					programCounter += 2;
+				}
+
+			} catch(Exception e)
 			{
-				//Draw
-				int x = Int32.Parse(opCode.Substring(1, 1));
-				int y = Int32.Parse(opCode.Substring(2, 1));
-				char n =  opCode[3];
-
-				Console.WriteLine("Draw " + n + " at x:"+x+", y:"+y);
-
-
-				display[x,y] = n;
-
+				Console.WriteLine("Error: " + e);
 			}
-			else
-			{
-				Console.WriteLine("Unknown opCode: "+ opCode);
-			}
-
-
-
-
-			if (!jumped)
-			{
-
-				programCounter += 2;
-			}
-
-
 
 			Console.ReadLine();
 
@@ -166,7 +179,14 @@ namespace Chip8
 		void readRom(string filePath)
 		{
 
-			memory = File.ReadAllBytes(filePath);
+			byte[] rom = File.ReadAllBytes(filePath);
+
+			//load rom at 512
+			Array.Copy(rom, 0, memory, 512, rom.Length);
+
+
+			Console.WriteLine("Rom Size: " + rom.Length);
+			Console.WriteLine("Memory Size: " + memory.Length);
 
 		}
 
